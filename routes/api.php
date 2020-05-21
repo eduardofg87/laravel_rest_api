3fba83d8bcaf65f84d1979578760d8e57b16dc28
Eduardo <goncalves.ef@gmail.com>
2020-05-21 13:06:49 -0300

    created products migration, model Product and /products API route

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Product;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/test', function (){
+    return ['msg' => 'minha primeira resposta de API'];
+});
+
+Route::get('/products', function (){
+    return Product::all();
 });
